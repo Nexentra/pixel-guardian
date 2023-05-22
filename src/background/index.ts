7c2feb4b1390b463a9b891072bf08531030370bd
towhid <ktowhid20@gmail.com>
2023-05-22 21:03:34 +0600

    commited unused code for now. also removing canvas code

--- src/background/index.ts
+++ src/background/index.ts
@@ -8,78 +8,78 @@ let nsfwSpy;
 // };
 
 
-chrome.windows.onCreated.addListener(async () => {
-  console.log("window created");
-  // const result = await ImageVerifier("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80");
-  // console.log(result);
-});
+// chrome.windows.onCreated.addListener(async () => {
+//   console.log("window created");
+//   // const result = await ImageVerifier("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80");
+//   // console.log(result);
+// });
 
-chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
-  // await chrome.declarativeNetRequest.updateDynamicRules({
-  //   removeRuleIds: [
+// chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, tab) {
+//   // await chrome.declarativeNetRequest.updateDynamicRules({
+//   //   removeRuleIds: [
       
-  //   ],
-  //   addRules: [
-  //     {
-  //       id: 1,
-  //       priority: 1,
-  //       action: {
-  //         type: "redirect",
-  //         redirect: {
-  //           url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR34hPo9zGkYxB2NKePgvPeImdm-CDTsHPrt4DFUyU_4A&s",
-  //         },
-  //       },
-  //       condition: {
-  //         resourceTypes: ["image"],
-  //       },
-  //     },
-  //   ],
-  // },
-  // (rules) => {
-  //   console.log("rules",rules);
-  //   });
-  if (changeInfo.status == 'loading') {
-    chrome.scripting
-    .executeScript({
-      target : {tabId : tab.id},
-      func : updateRules,
-    })
-    .then(async () => {
-      console.log("injected");
-    });
-  }
-})
+//   //   ],
+//   //   addRules: [
+//   //     {
+//   //       id: 1,
+//   //       priority: 1,
+//   //       action: {
+//   //         type: "redirect",
+//   //         redirect: {
+//   //           url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR34hPo9zGkYxB2NKePgvPeImdm-CDTsHPrt4DFUyU_4A&s",
+//   //         },
+//   //       },
+//   //       condition: {
+//   //         resourceTypes: ["image"],
+//   //       },
+//   //     },
+//   //   ],
+//   // },
+//   // (rules) => {
+//   //   console.log("rules",rules);
+//   //   });
+//   if (changeInfo.status == 'loading') {
+//     chrome.scripting
+//     .executeScript({
+//       target : {tabId : tab.id},
+//       func : updateRules,
+//     })
+//     .then(async () => {
+//       console.log("injected");
+//     });
+//   }
+// })
 
 
-async function updateRules(){
-  console.log("injected");
-}
+// async function updateRules(){
+//   console.log("injected");
+// }
 
 
-chrome.runtime.onInstalled.addListener(() => {
-  // storage.get().then(
-  //     (storage) => {
-  //         nsfwSpy = storage.nsfwSpy
-  //     }
-  // );
-});
+// chrome.runtime.onInstalled.addListener(() => {
+//   // storage.get().then(
+//   //     (storage) => {
+//   //         nsfwSpy = storage.nsfwSpy
+//   //     }
+//   // );
+// });
 
-chrome.runtime.onInstalled.addListener(async () => {
-  // Load the declarative net request rules
-  await chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [],
-    addRules: [],
-  },
-  (rules) => {
-    console.log("rules",rules);
-    });     
+// chrome.runtime.onInstalled.addListener(async () => {
+//   // Load the declarative net request rules
+//   await chrome.declarativeNetRequest.updateDynamicRules({
+//     removeRuleIds: [],
+//     addRules: [],
+//   },
+//   (rules) => {
+//     console.log("rules",rules);
+//     });     
 
-  // Optional: Log the updated rules
-  const rules = await chrome.declarativeNetRequest.getDynamicRules();
-  console.log(rules);
+//   // Optional: Log the updated rules
+//   const rules = await chrome.declarativeNetRequest.getDynamicRules();
+//   console.log(rules);
   
-  // ImageVerifier("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80")
-});
+//   // ImageVerifier("https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80")
+// });
 
 async function ImageVerifier(imageUrl) {
   nsfwSpy = new NsfwSpy("./model/model.json");
@@ -101,41 +101,37 @@ function addCommandHandler() {
             image.setAttribute('crossorigin', 'anonymous');
             console.log('Censor image: ', image);
 
+            image.style.filter = 'blur(3px)';
+
             // Create a canvas element
-            const canvas = document.createElement('canvas');
-            const context = canvas.getContext('2d');
-
-            // Wait for the image to load before setting the canvas dimensions and drawing the image
-            image.addEventListener('load', () => {
-              // Set the canvas dimensions to match the image
-              canvas.width = image.width;
-              canvas.height = image.height;
-
-              // Draw the image on the canvas
-              context.drawImage(image, 0, 0, canvas.width, canvas.height);
-
-              // Apply the blur effect
-              const blurAmount = 20; // Adjust the blur amount as desired
-              context.filter = `blur(${blurAmount}px)`;
-
-              // Draw the blurred image on the canvas
-              context.drawImage(canvas, 0, 0, canvas.width, canvas.height);
-
-              // Replace the image with the blurred canvas
-              if (image.srcset) {
-                image.srcset = canvas.toDataURL();
-              }
-              if (image.src) {
-                image.src = canvas.toDataURL();
-              }
-
-              console.log('Censor image: ', image.src);
-            });
+            // const canvas = document.createElement('canvas');
+            // const context = canvas.getContext('2d');
 
             // Check if the image is already loaded
-            if (image.complete) {
-              image.dispatchEvent(new Event('load'));
-            }
+            // if (image.complete) {
+            //   canvas.width = image.width;
+            //   canvas.height = image.height;
+  
+            //   // Draw the image on the canvas
+            //   context.drawImage(image, 0, 0, canvas.width, canvas.height)
+  
+            //   // Apply the blur effect
+            //   const blurAmount = 20; // Adjust the blur amount as desired
+            //   context.filter = `blur(${blurAmount}px)`;
+  
+            //   // Draw the blurred image on the canvas
+            //   context.drawImage(canvas, 0, 0, canvas.width, canvas.height);
+  
+            //   // // Replace the image with the blurred canvas
+            //   if (image.srcset) {
+            //     image.srcset = canvas.toDataURL();
+            //   }
+            //   if (image.src) {
+            //     image.src = canvas.toDataURL();
+            //   }
+  
+            //   console.log('Censored image: ', image.src);
+            // }
           });
         }})
         return
